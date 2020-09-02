65a07aeef6383e36f9021e4157e2ed4dff332f7a
Gonzalo Sola <gonzalosola@gmail.com>
2020-09-02 01:27:25 -0300

    cambios

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 function cambiarMensaje(id) {
   document.getElementById(""+id+"").innerHTML = "Paragraph changed.";
 }
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
